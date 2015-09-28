d59803ee2020bfa479225a0201f02c901a19937d
Marvin <student@epicodus.com>
2015-09-28 12:25:51 -0700

    custom directives work

--- controllers/FreelancersController.js
+++ controllers/FreelancersController.js
@@ -1,10 +1,8 @@
 freelancer.controller('FreelancersCtrl', function FreelancersCtrl($scope) {
-  $scope.courses = CoursesFactory.courses;
-  $scope.CoursesFactory = CoursesFactory;
+  
   $scope.ctrlCourse = "shoot"
 
   $scope.alert = function(message) {
     alert(message)
   }
-
 });
